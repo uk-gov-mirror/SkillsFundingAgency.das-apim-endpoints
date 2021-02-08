cde003734dd97dde8be05570e29c98a2a483d0d1
Ben Arnold <ben@arnoldfamily.me.uk>
2021-02-08 11:42:54 +0000

    Log out response from Commitments API

--- src/SFA.DAS.ApprenticeCommitments/Application/Services/CommitmentsV2Service.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Services/CommitmentsV2Service.cs
@@ -1,8 +1,9 @@
-﻿using SFA.DAS.ApprenticeCommitments.Apis.CommitmentsV2InnerApi;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SFA.DAS.ApprenticeCommitments.Apis.CommitmentsV2InnerApi;
 using SFA.DAS.ApprenticeCommitments.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
 using SFA.DAS.SharedOuterApi.Interfaces;
-using System;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeCommitments.Application.Services
@@ -10,15 +11,29 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Services
     public class CommitmentsV2Service
     {
         private readonly IInternalApiClient<CommitmentsV2Configuration> _client;
+        private readonly ILogger<CommitmentsV2Service> _logger;
 
-        public CommitmentsV2Service(IInternalApiClient<CommitmentsV2Configuration> client) => _client = client;
+        public CommitmentsV2Service(
+            IInternalApiClient<CommitmentsV2Configuration> client,
+            ILogger<CommitmentsV2Service> logger) =>
+            (_client, _logger) = (client, logger);
 
         public async Task<ApprenticeshipResponse> GetApprenticeshipDetails(long accountId, long apprenticeshipId)
         {
             var apprenticeship = await _client.Get<ApprenticeshipResponse>(
                 new GetApprenticeshipDetailsRequest(apprenticeshipId));
 
-            if (apprenticeship?.EmployerAccountId != accountId)
+            if (apprenticeship == null)
+            {
+                throw new HttpRequestContentException(
+                    $"Apprenticeship Id {apprenticeshipId} not found",
+                    System.Net.HttpStatusCode.BadRequest,
+                    "");
+            }
+
+            _logger.LogInformation($"Entire response: {JsonConvert.SerializeObject(apprenticeship, Formatting.Indented)}");
+
+            if (apprenticeship.EmployerAccountId != accountId)
             {
                 throw new HttpRequestContentException(
                     $"Employer Account {accountId} does not have access to apprenticeship Id {apprenticeshipId}",
